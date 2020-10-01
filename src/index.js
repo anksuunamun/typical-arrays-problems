91f4c2675c0684fc61d3f0e61930d6d6758e7704
anksuunamun <shishkina_veronika_10@mail.ru>
2020-10-01 17:48:26 +0300

    feat: add min, max and avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,48 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (Array.isArray(array) && array.length !== 0) {
+        for (let item of array) {
+            if (typeof item !== "number") {
+                return 0;
+            }
+        }
+        let temporaryMin = array[0];
+        for (let item of array) {
+            if (item < temporaryMin) {
+                temporaryMin = item;
+            }
+        }
+        return temporaryMin;
+    } else return 0;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (Array.isArray(array) && array.length !== 0) {
+        for (let item of array) {
+            if (typeof item !== "number") {
+                return 0;
+            }
+        }
+        let temporaryMax = array[0];
+        for (let item of array) {
+            if (item > temporaryMax) {
+                temporaryMax = item;
+            }
+        }
+        return temporaryMax;
+    } else return 0;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (Array.isArray(array) && array.length !== 0) {
+        for (let item of array) {
+            if (typeof item !== "number") {
+                return 0;
+            }
+        }
+        let average = 0;
+        for (let item of array) {
+            average += item;
+        }
+        return average / array.length;
+    } else return 0;
 }
